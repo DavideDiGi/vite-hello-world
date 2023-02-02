e36ae1fa9bbf2371408020d6148f4ec6988cb374
Davide Di Girolamo <davide.digi2001@gmail.com>
2023-02-02 09:36:31 +0100

    aggiunto file AppTitle.vue

---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'La mia prima app con Vite!'
+        }
+    }
+}
+
+</script>
+
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+    color: #888;
+}
+</style>
